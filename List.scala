293e213451bf6a8a5a61b6242f22bd7c003abe50
MeerKatDev <lcampobasso@gmail.com>
2013-08-27 21:39:05 +0200

    updates
    problems from 13 to 26

--- List.scala
+++ List.scala
@@ -22,10 +22,16 @@ object List {
   
   // P05: Reverse a list 
   // reverse(List(1, 1, 2, 3, 5, 8))
-  // Note: I don't know how to use MapConserve here, to correct
-  // numbers.map( s=> sub( numbers.length-1-sub.indexOf(s) ) )
-  def reverse(numbers: List[Int]): List[Int] = numbers.reverse
-  
+  //   => List[Int] = List(8, 5, 3, 2, 1, 1)
+  def reverse(numbers: List[_]): List[_] = {	  
+    def recurse(list: List[_], acc: List[_], n: Int): List[_] = list match {
+      case Nil => acc     
+      case head::tail if(n==0) => acc 
+      case head::tail => recurse(tail,head::acc, n-1)      
+    }
+    recurse(numbers, List(), numbers.size)
+  }
+    
   // P06: Find out whether a list is a palindrome.
   // isPalindrome(List(1, 2, 3, 2, 1)) => true
   def isPalindrome(numbers: List[Int]) = 
@@ -104,6 +110,210 @@ object List {
       case (a,b)::tail => recurse((if(a>1) (a-1,b)::tail else tail), b::acc)
     }
     recurse(list.reverse,List[Char]())
+  }
+  
+  // P13: Run-length encoding of a list (direct solution). DUPLICATED
+  // Implement the so-called run-length encoding data compression method directly. 
+  // I.e. don't use other methods you've written (like P09's pack); do all the work directly.
+  // encodeDirect(List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e'))
+  //   => List[(Int, Char)] = List((4, 'a'), (1, 'b'), (2, 'c'), (2, 'a'), (1, 'd'), (4, 'e'))
+  def encodeDirect(list: List[Char]): List[(Int,Char)] = {
+    def recurse(list: List[Char], prev: Char, acc: List[(Int,Char)]): List[(Int,Char)] = list match {
+      case Nil => acc
+      case h::tail if h == prev => recurse(tail,h,(acc.head._1+1,h)::acc.tail)
+      case h::tail => recurse(tail, h, (1,h)::acc)
+    }
+    recurse( list.init.reverse, list.last, List[(Int,Char)]((1,list.last))) 
+  } 
+  
+  // P14 (*) Duplicate the elements of a list.
+  // duplicate(List('a', 'b', 'c', 'c', 'd'))
+  //   => List[Symbol] = List('a', 'a', 'b', 'b', 'c', 'c', 'c', 'c', 'd', 'd')
+  def duplicate(list: List[Char]): List[Char] = {
+    def recurse(init: List[Char], acc: List[Char]): List[Char] = init match {
+      case Nil => acc
+      case a::tail => recurse(tail,a::a::acc)
+    }
+    recurse(list.init.reverse,List(list.last,list.last) )
+  }  
+  // P15: Duplicate the elements of a list a given number of times.
+  // duplicateN(3, List('a, 'b, 'c, 'c, 'd))
+  //    => List[Symbol] = List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd)
+  def duplicateN(Num: Int, list: List[Symbol]):List[Symbol] = {
+    def recurse(init: List[Symbol], acc: List[Symbol], n: Int): List[Symbol] = init match {
+      case Nil => acc
+      case a::tail if n>1 => recurse(a::tail,a::acc, n-1)
+      case a::tail if n==1 => recurse(tail,a::acc, Num)
+    }
+    recurse(list.reverse,List[Symbol](), Num)    
+  } 
+  
+  // P16: Drop every Nth element from a list.
+  // drop(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  //   => List[Symbol] = List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k)
+  def drop(Num: Int, list: List[Symbol]): List[Symbol] = {
+    def recurse(init: List[Symbol], acc: List[Symbol], n: Int): List[Symbol] = init match {
+      case Nil => acc
+      case a::tail if n>1 => recurse(tail,a::acc, n-1)
+      case a::tail if n==1 => recurse(tail,acc, Num)
+    }
+    recurse(list.reverse,List[Symbol](), Num)    
+  }
+  
+  // P17: Split a list into two parts.
+  // The length of the first part is given. Use a Tuple for your result.
+  // split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  //   => (List[Symbol], List[Symbol]) = (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  def split(Num: Int, list: List[Symbol]): (List[Symbol], List[Symbol]) = {
+    def recurse(init: List[Symbol], acc: (List[Symbol], List[Symbol]), n: Int): (List[Symbol], List[Symbol]) = init match {
+      case Nil => acc.swap
+      case a::tail if n>0 => recurse(tail,(a::acc._1,acc._2), n-1)
+      case a::tail if n==0 => recurse(tail,(acc._1,a::acc._2), 0)
+    }
+    recurse(list.reverse,(List[Symbol](), List[Symbol]()), list.length-Num)    
+  }
+ 
+  // P18: Extract a slice from a list.
+  // Given two indices, I and K, the slice is the list containing the elements
+  // from and including the Ith element up to but not including the Kth element of the original list.
+  // Start counting the elements with 0.
+  // slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  //   => List[Symbol] = List('d, 'e, 'f, 'g)
+  def slice(start: Int, end: Int, list: List[Symbol]): List[Symbol] = {
+    def recurse(init: List[Symbol], acc: List[Symbol], n: Int): List[Symbol] = init match {
+      case Nil => acc
+      case a::tail if n>0 || acc.length == end-start => recurse(tail,acc, n-1)
+      case a::tail if n==0 => recurse(tail,a::acc, 0)
+    }
+    recurse(list.reverse,List[Symbol](), list.length-end)    
+  }
+  
+  // P19: Rotate a list N places to the left.
+  // rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  //   => List[Symbol] = List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c)
+  // rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  //   => List[Symbol] = List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i)
+  def rotate(num: Int, list: List[Symbol]): List[Symbol] = {
+    var l = (List[Symbol](), List[Symbol]())
+    if(num<0){       
+      l = split(-num,list.reverse).swap 
+      l = (l._1.reverse,l._2.reverse)
+    }
+    else{ l = split(num,list) }
+    l._2:::l._1
+  }
+
+  // P20: Remove the Kth element from a list.
+  // Return the list and the removed element in a Tuple. 
+  // Elements are numbered from 0.
+  // removeAt(1, List('a, 'b, 'c, 'd))
+  //   => (List[Symbol], Symbol) = (List('a, 'c, 'd),'b)
+  def removeAt(num: Int, list: List[Symbol]): (List[Symbol], Symbol) = {
+    def recurse(init: List[Symbol], acc: List[Symbol], n: Int): (List[Symbol], Symbol) = init match {
+      case a::tail if n>1 => recurse(tail,a::acc, n-1)
+      case a::tail if n==1 => (tail:::acc,a)
+    }
+    recurse(list.reverse,List[Symbol](), list.length-num)    
+  }
+  
+  // P21: Insert an element at a given position into a list.
+  // insertAt('new, 1, List('a, 'b, 'c, 'd))
+  //   => List[Symbol] = List('a, 'new, 'b, 'c, 'd)
+  def insertAt(new_symbol: Symbol, num: Int, list: List[Symbol]): List[Symbol] = {
+    def recurse(init: List[Symbol], acc: List[Symbol], n: Int): List[Symbol] = init match {
+      case a::tail if n>1 => recurse(tail,a::acc, n-1)
+      case a::tail if n==1 => (tail:::(new_symbol::a::acc))
+    }
+    recurse(list.reverse,List[Symbol](), list.length-num)    
+  }
+  // P22: Create a list containing all integers within a given range.
+  // range(4, 9)
+  //   => List[Int] = List(4, 5, 6, 7, 8, 9)
+  
+  def range(a: Int, b: Int): List[Int] = {
+    def recurse(acc: List[Int], n: Int): List[Int] = acc match {
+      case acc if n==b => n::acc
+      case acc => recurse(n::acc,n+1)
+    }
+    recurse(List[Int](), a).reverse    
+  }
+  
+  // P23: Extract a given number of randomly selected elements from a list.
+  // randomSelect(3, List('a, 'b, 'c, 'd, 'f, 'g, 'h))
+  //   => List[Symbol] = List('e, 'd, 'a)
+  // Hint: Use the solution to problem P20
+  def randomSelect(num: Int, list: List[Symbol]): List[Symbol] = {
+    val r = new Random
+    @tailrec def recurse(acc: List[Symbol],n: Int): List[Symbol] = {
+      if(n==0) acc
+      else {
+        val removed = removeAt(r.nextInt(list.size),list)._2
+        if(acc contains removed)
+          recurse(acc, n)
+        else
+          recurse(removed::acc, n-1)
+      }
+    }
+    recurse(List[Symbol](),num)         
+  }
+  
+  // P24: Lotto: Draw N different random numbers from the set 1..M.
+  // lotto(6, 49)
+  //   => List[Int] = List(23, 1, 17, 33, 21, 37)
+  // Hint: Use the solution of problem P23.
+  def lotto(howMany: Int, _interval: Int): List[Int] = {
+    val interval = range(1,_interval)
+    val r = new Random
+    def remove(num: Int, list: List[Int]): (List[Int], Int) = {
+      def recurse(init: List[Int], acc: List[Int], n: Int): (List[Int], Int) = init match {
+        case a::tail if n>1 => recurse(tail,a::acc, n-1)
+        case a::tail if n==1 => (tail:::acc,a)
+      }
+      recurse(list.reverse,List[Int](), list.length-num)    
+    }
+    @tailrec def recurse(acc: List[Int],n: Int): List[Int] = {
+      if(n==0) acc
+      else {
+        val removed = remove(r.nextInt(interval.size),interval)._2
+        if(acc contains removed)
+          recurse(acc, n)
+        else
+          recurse(removed::acc, n-1)
+      }
+    }
+    recurse(List[Int](),howMany)
+  }
+  
+  // P25: Generate a random permutation of the elements of a list.
+  // randomPermute(List('a, 'b, 'c, 'd, 'e, 'f))
+  //   => List[Symbol] = List('b, 'a, 'd, 'c, 'e, 'f)  
+  def randomPermute(list: List[Symbol]): List[Symbol] = {
+    randomSelect(list.size,list).reverse
+  }
+  println(randomPermute(List('a, 'b, 'c, 'd, 'e, 'f)))
+  
+  // P26: Generate the combinations of K distinct objects chosen from the N elements of a list.
+  // In how many ways can a committee of 3 be chosen from a group of 12 people? 
+  // We all know that there are C(12,3) = 220 possibilities (C(N,K) denotes the well-known binomial coefficient). 
+  // For pure mathematicians, this result may be great. But we want to really generate all the possibilities.
+  // combinations(3, List('a, 'b, 'c, 'd, 'e, 'f))
+  //   => List[List[Symbol]] = List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), ...
+  def combinations(num: Int, list: List[Symbol]): List[List[Symbol]] = { 
+    def binomialCoefficient(n:Int, k:Int)=fact(n) / (fact(k) * fact(n-k))
+    def fact(n:Int):Int=if (n==0) 1 else n*fact(n-1)   
+    
+    @tailrec def recurse(acc : List[List[Symbol]], n: Int): List[List[Symbol]] = {
+      if(n==0) acc
+      else {
+        val gen = randomSelect(num,list)
+        if(acc contains gen)
+          recurse(acc, n)
+        else
+          recurse(gen::acc, n-1)
+      }
+    }
+
+    recurse( List[List[Symbol]](),binomialCoefficient(list.size,num)) 
   }
   
 } 
